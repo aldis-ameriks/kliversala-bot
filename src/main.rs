363d7276b216936ed61aae547ca90572b2aaaf5d
Aldis Ameriks <aldis.ameriks@gmail.com>
2020-02-01 21:30:48 +0200

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -33,19 +33,12 @@ fn process_posts() -> Result<(), Box<dyn Error>> {
 
     for post in posts {
         match client.get_post(&post.id)? {
-            Some(_) => {
-                info!("post already sent: {}", &post.id);
-                continue;
-            }
+            Some(_) => info!("post already sent: {}", &post.id),
             None => {
-                info!("post is not already sent: {}", &post.id);
                 info!("sending notification for post: {:#?}", post);
                 match send_message(&post.text) {
                     Err(e) => error!("failed to send message {}", e),
-                    Ok(()) => {
-                        client.put_post(&post)?;
-                        continue;
-                    }
+                    Ok(()) => client.put_post(&post)?
                 }
             }
         }
