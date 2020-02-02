c8d5000d4ac4bae2e69b43b6e8e82abdbcb038a8
Aldis Ameriks <aldis.ameriks@gmail.com>
2020-02-02 19:46:24 +0200

    Restructure telegram client and add tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::error::Error;
 
 use lambda_runtime::{error::HandlerError, lambda, Context};
@@ -7,7 +8,7 @@ use serde_json::Value;
 
 use db::Client;
 use posts::fetch_posts;
-use telegram::{send_image, send_message};
+use telegram::TelegramClient;
 
 mod db;
 mod posts;
@@ -16,6 +17,10 @@ mod telegram;
 fn main() {
     simple_logger::init_with_level(Level::Info).expect("Failed to init logger");
     lambda!(handler)
+    //    match process_posts() {
+    //        Ok(()) => info!("successfully processed posts"),
+    //        Err(e) => error!("error occurred while processing posts: {}", e),
+    //    }
 }
 
 fn handler(event: Value, _: Context) -> Result<Value, HandlerError> {
@@ -27,17 +32,22 @@ fn handler(event: Value, _: Context) -> Result<Value, HandlerError> {
 }
 
 fn process_posts() -> Result<(), Box<dyn Error>> {
+    let token = env::var("TG_TOKEN").expect("Missing TG_TOKEN env var");
+    let chat_id = env::var("TG_CHAT_ID").expect("Missing TG_CHAT_ID env var");
+
     let client = Client::new();
+    let telegram_client = TelegramClient::new(token, chat_id);
+
     let posts = fetch_posts()?;
     info!("found {} posts", posts.len());
 
     for post in posts {
         if let None = client.get_post(&post.id)? {
             info!("sending notification for post: {:?}", post);
-            send_message(&post.text)?;
+            telegram_client.send_message(&post.text)?;
             client.put_post(&post)?;
             for image in post.images {
-                send_image(&image)?;
+                telegram_client.send_image(&image)?;
             }
         } else {
             info!("post is already sent: {}", &post.id)
--- src/telegram.rs
+++ src/telegram.rs
@@ -1,8 +1,13 @@
-use std::env;
 use std::error::Error;
 
 use reqwest::blocking::{Client, Response};
 use serde::Serialize;
+
+pub struct TelegramClient {
+    token: String,
+    chat_id: String,
+    domain: String,
+}
 
 #[derive(Serialize)]
 struct Message<'a> {
@@ -11,58 +16,141 @@ struct Message<'a> {
     disable_notification: bool,
 }
 
-pub fn send_message(text: &str) -> Result<(), Box<dyn Error>> {
-    let token = env::var("TG_TOKEN").expect("Missing TG_TOKEN env var");
-    let chat_id = env::var("TG_CHAT_ID").expect("Missing TG_CHAT_ID env var");
-    let message = Message {
-        chat_id: chat_id.as_str(),
-        text,
-        disable_notification: true,
-    };
+impl TelegramClient {
+    pub fn new(token: String, chat_id: String) -> TelegramClient {
+        TelegramClient {
+            token,
+            chat_id,
+            domain: String::from("https://api.telegram.org"),
+        }
+    }
 
-    let url = format!("https://api.telegram.org/bot{}/sendMessage", token);
-    let resp: Response = Client::builder()
-        .build()?
-        .post(&url)
-        .json(&message)
-        .send()?;
+    pub fn new_with(token: String, chat_id: String, domain: String) -> TelegramClient {
+        TelegramClient {
+            token,
+            chat_id,
+            domain,
+        }
+    }
 
-    if resp.status().is_success() {
-        Ok(())
-    } else {
-        Err(resp.text()?.into())
+    pub fn send_message(&self, text: &str) -> Result<(), Box<dyn Error>> {
+        let message = Message {
+            chat_id: &self.chat_id,
+            text,
+            disable_notification: true,
+        };
+
+        let url = format!("{}/bot{}/sendMessage", self.domain, self.token);
+        let resp: Response = Client::builder()
+            .build()?
+            .post(&url)
+            .json(&message)
+            .send()?;
+
+        if resp.status().is_success() {
+            Ok(())
+        } else {
+            Err(resp.text()?.into())
+        }
+    }
+
+    pub fn send_image(&self, url: &str) -> Result<(), Box<dyn Error>> {
+        let params = [("chat_id", self.chat_id.as_str()), ("photo", url)];
+        let url = format!("{}/bot{}/sendPhoto", self.domain, self.token);
+        let resp: Response = Client::builder().build()?.post(&url).form(&params).send()?;
+
+        if resp.status().is_success() {
+            Ok(())
+        } else {
+            Err(resp.text()?.into())
+        }
     }
 }
-
-pub fn send_image(url: &str) -> Result<(), Box<dyn Error>> {
-    let token = env::var("TG_TOKEN").expect("Missing TG_TOKEN env var");
-    let chat_id = env::var("TG_CHAT_ID").expect("Missing TG_CHAT_ID env var");
-    let params = [("chat_id", chat_id.as_str()), ("photo", url)];
-    let url = format!("https://api.telegram.org/bot{}/sendPhoto", token);
-    let resp: Response = Client::builder().build()?.post(&url).form(&params).send()?;
-
-    if resp.status().is_success() {
-        Ok(())
-    } else {
-        Err(resp.text()?.into())
-    }
-}
-
-
 
 #[cfg(test)]
 mod tests {
+    use mockito;
+
     use super::*;
 
+    const TOKEN: &str = "token";
+    const CHAT_ID: &str = "123";
+
     #[test]
-    #[should_panic(expected = "Missing TG_TOKEN env var")]
-    fn send_message_missing_token() {
-        send_message("message").unwrap();
+    fn send_message_success() {
+        let url = &mockito::server_url();
+        let _m = mockito::mock("POST", format!("/bot{}/sendMessage", TOKEN).as_str())
+            .with_status(200)
+            .with_header("content-type", "application/json")
+            .create();
+
+        let client = TelegramClient::new_with(
+            String::from(TOKEN),
+            String::from(CHAT_ID),
+            String::from(url),
+        );
+
+        let result = client.send_message("test").unwrap();
+        assert_eq!(result, ());
     }
 
     #[test]
-    #[should_panic(expected = "Missing TG_TOKEN env var")]
-    fn send_image_missing_token() {
-        send_image("image url").unwrap();
+    fn send_message_error() {
+        let error = r#"{"ok":false,"error_code":400,"description":"Bad Request: chat not found"}"#;
+        let url = &mockito::server_url();
+        let _m = mockito::mock("POST", format!("/bot{}/sendMessage", TOKEN).as_str())
+            .with_status(400)
+            .with_header("content-type", "application/json")
+            .with_body(error)
+            .create();
+
+        let client = TelegramClient::new_with(
+            String::from(TOKEN),
+            String::from(CHAT_ID),
+            String::from(url),
+        );
+
+        let result = client.send_message("test").unwrap_err();
+        let result = format!("{}", result);
+        assert_eq!(result, error);
+    }
+
+    #[test]
+    fn send_image_success() {
+        let url = &mockito::server_url();
+        let _m = mockito::mock("POST", format!("/bot{}/sendPhoto", TOKEN).as_str())
+            .with_status(200)
+            .with_header("content-type", "application/json")
+            .create();
+
+        let client = TelegramClient::new_with(
+            String::from(TOKEN),
+            String::from(CHAT_ID),
+            String::from(url),
+        );
+
+        let result = client.send_image("image url").unwrap();
+        assert_eq!(result, ());
+    }
+
+    #[test]
+    fn send_image_error() {
+        let error = r#"{"ok":false,"error_code":400,"description":"Bad Request: chat not found"}"#;
+        let url = &mockito::server_url();
+        let _m = mockito::mock("POST", format!("/bot{}/sendPhoto", TOKEN).as_str())
+            .with_status(400)
+            .with_header("content-type", "application/json")
+            .with_body(error)
+            .create();
+
+        let client = TelegramClient::new_with(
+            String::from(TOKEN),
+            String::from(CHAT_ID),
+            String::from(url),
+        );
+
+        let result = client.send_image("image url").unwrap_err();
+        let result = format!("{}", result);
+        assert_eq!(result, error);
     }
 }
