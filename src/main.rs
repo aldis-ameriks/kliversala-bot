bc7069dcc6bd082429cb814230aeb29ec69ebb8f
Aldis Ameriks <aldis.ameriks@gmail.com>
2020-02-15 00:59:54 +0200

    Use closure

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,14 @@ use kliversala_bot::process_posts;
 
 fn main() {
     simple_logger::init_with_level(Level::Info).expect("Failed to init logger");
-    lambda!(handler);
-}
-
-fn handler(event: Value, _: Context) -> Result<Value, HandlerError> {
-    let mut rt = Runtime::new().unwrap();
-    rt.block_on(async {
-        match process_posts().await {
-            Ok(()) => info!("successfully processed posts"),
-            Err(e) => error!("error occurred while processing posts: {}", e),
-        }
+    lambda!(|event: Value, _: Context| -> Result<Value, HandlerError> {
+        let mut rt = Runtime::new().unwrap();
+        rt.block_on(async {
+            match process_posts().await {
+                Ok(()) => info!("successfully processed posts"),
+                Err(e) => error!("error occurred while processing posts: {}", e),
+            }
+        });
+        Ok(event)
     });
-    Ok(event)
 }
